3e901e0f4a33467bc7cb6dee092981ca34eaa64b
Anderson Silva <andssilva.23@gmail.com>
2024-01-29 12:19:52 -0400

    feat(cap 6) :rocket: exercício 8

---
+++ estrutura-repetitiva/src/exercicio8/Program.java
@@ -0,0 +1,23 @@
+package exercicio8;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int total = 1;
+		
+		for(int i = 1; i <= N; i++) {
+			total *= i;
+		}
+		
+		System.out.println(total);
+		
+		sc.close();
+
+	}
+
+}
